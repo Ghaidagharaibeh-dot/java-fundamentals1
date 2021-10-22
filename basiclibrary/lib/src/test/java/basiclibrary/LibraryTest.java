78aa56ca4c4f82d6bc040891b24819054421a009
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-10-22 22:46:44 +0300

    lab 3

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -5,6 +5,10 @@ package basiclibrary;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
@@ -33,6 +37,39 @@ assertTrue(testing.containsDuplicates(arr1));
         };
         int[] exppectedValue={55, 54, 60, 53, 59, 57, 61};
         assertArrayEquals(exppectedValue,testing.lowestaverage(arr));
+    }
+    @Test public void teatWeatherData (){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String expected =
+              "Never saw temperature: 59\n" +
+                      "Never saw temperature: 61\n" +
+                      "Never saw temperature: 62\n" +
+                      "Never saw temperature: 63\n" +
+                      "Never saw temperature: 67\n" +
+                      "Never saw temperature: 68\n" +
+                      "Never saw temperature: 69\n" +
+                      "Max Value =70Min Value =52";
+
+
+        assertEquals(expected,testing.weatherData(weeklyMonthTemperatures));
+    }
+    @Test public void testTally(){
+        List<String> votes  = new ArrayList<>();
+        votes .add("Bush");
+        votes .add("Bush");
+        votes .add("Bush");
+        votes .add("Shrub");
+        votes .add("Hedge");
+        votes .add("Shrub");
+        votes .add("Bush");
+        votes .add("Hedge");
+        votes .add("Bush");
+        assertEquals("Bush",testing.tally(votes));
     }
 
     }
